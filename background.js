46b2003b333ababf2ba8f3088767fec87f65a034
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-29 18:44:45 -0400

    feat: created content script listening logic in the background script

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-console.log('background.js');
+// Listeners
+// Listen for messages from tabs
+browser.runtime.onMessage.addListener(function(message, sender) {
+  // Validate message structure
+  if (message.from === 'content' && message.subject === 'showPageAction') {
+    // Enable page action for requesting tab
+    browser.pageAction.show(sender.tab.id);
+  }
+});
